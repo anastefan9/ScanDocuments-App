b914825d9ce749b6ce9f0fa401d4a258dacb7d2e
Ana Stefan <ana.stefan@mready.net>
2024-01-07 15:26:42 +0200

    Initial Commit

---
+++ ScanDocuments App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScanDocuments App
+//
+//  Created by Ana Ștefan on 07.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScanDocuments App/ScanDocuments_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanDocuments_AppApp.swift
+//  ScanDocuments App
+//
+//  Created by Ana Ștefan on 07.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScanDocuments_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
